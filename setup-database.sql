a184bb25162fd34579d7d52935c0497136bfdb96
Ezra Collins <igobykobe@gmail.com>
2022-09-11 20:55:47 +1300

    Updated setup-database.sql to create table for assets

--- setup-database.sql
+++ setup-database.sql
@@ -1,18 +1,13 @@
-DROP TABLE IF EXISTS papers;
-
 CREATE TABLE assets (
   assetID INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
   assetType varchar(50) NOT NULL,
   brand varchar(20),
   modelno varchar(20),
   serialno varchar(20),
-  datepurchased varchar(10),
-  location varchar(10),
-  PRIMARY KEY (assetID)
+  datepurchased date NOT NULL,
+  location varchar(10)
 )
-AUTO_INCREMENT 0001;
+AUTO_INCREMENT=0001;
 
-INSERT INTO assets (assetID, assetType, brand, modelno, serialno, datepurchased, location)
-	VALUES ('0001','CPU','Dell','Optiplex 780', 'DGK8H63','07/08/2022','Level 1');
-INSERT INTO assets (assetID, assetType, brand, modelno, serialno, datepurchased, location)
-	VALUES ('0002','Mouse','Lenovo','M-U0025-O', 'LZ123330BGC','28/06/2022','Level 3');
+INSERT INTO assets (assetType, brand, modelno, serialno, datepurchased, location) VALUES ('CPU','Dell','Optiplex 780', 'DGK8H63','07/08/2022','Level 1');
+INSERT INTO assets (assetType, brand, modelno, serialno, datepurchased, location) VALUES ('Mouse','Lenovo','M-U0025-O', 'LZ123330BGC','28/06/2022','Level 2');
